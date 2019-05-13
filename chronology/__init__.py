7ea40a9700208ada05a69135fe3e262c32c56d84
Idin <d@idin.net>
2019-05-13 00:28:44 -0700

    add new stuff to init

--- chronology/__init__.py
+++ chronology/__init__.py
@@ -3,7 +3,9 @@ from .to_quarter import get_quarter
 from .get_month import get_month_num, get_month_name, get_months_list
 from .add_time import add_days, add_weeks, add_months, add_quarters, add_years, add_time
 from .get_time import get_now, get_today, get_today_str, sleep, get_time, get_date
-from .get_elapsed import get_elapsed_seconds, get_elapsed, get_elapsed_days
+from .get_elapsed import get_elapsed, convert
 from .YearMonth import YearMonth
-from .Timer import Timer
 from .get_number_of_days import get_number_of_days
+from .Timer_class import Timer
+from .MeasurementSet_class import MeasurementSet
+from .measure_function import measure
