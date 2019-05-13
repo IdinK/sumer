a8b0de8cbba6614040754236a5b6dede83bb335a
Idin <d@idin.net>
2019-05-13 00:28:19 -0700

    add docstring

--- chronology/get_time.py
+++ chronology/get_time.py
@@ -3,14 +3,23 @@ import time as _time
 
 
 def get_today():
+	"""
+	:rtype: date
+	"""
 	return date.today()
 
 
 def get_today_str():
+	"""
+	:rtype:  str
+	"""
 	return str(get_today())
 
 
 def get_now():
+	"""
+	:rtype: datetime
+	"""
 	return datetime.now()
 
 
